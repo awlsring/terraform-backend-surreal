0b0d5fe91f29f68438ca7a4396f8e749a660fbf9
awlsring <mattcanemail@gmail.com>
2023-01-12 22:21:03 -0800

    fix: load sensitive values from env

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,12 +5,23 @@ import (
 
 	"github.com/awlsring/surreal-db-client/surreal"
 	"github.com/spf13/viper"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type Config struct {
 	Port int `mapstructure:"port"`
 	Users map[string]string `mapstructure:"users"`
 	Surreal surreal.SurrealConfig `mapstructure:"surreal"`
+}
+
+type ConfigMap struct {
+	Port int `mapstructure:"port"`
+	Surreal surreal.SurrealConfig `mapstructure:"surreal"`
+}
+
+type UserMap struct {
+	Users map[string]string `mapstructure:"users"`
 }
 
 func getConfigPath() string {
@@ -22,19 +33,79 @@ func getConfigPath() string {
 	return "/config/config.yaml"
 }
 
-func LoadConfig() (Config, error) {
+func getUsersPath() string {
+	path := os.Getenv("USERS_PATH")
+	if path != "" {
+		return path
+	}
+
+	return "/config/users.yaml"
+}
+
+func LoadConfigMap() (ConfigMap, error) {
 	vp := viper.New()
 	vp.SetConfigFile(getConfigPath())
 
-	var config Config
+	var config ConfigMap
 	err := vp.ReadInConfig()
 	if err != nil {
-		return Config{}, err
+		return ConfigMap{}, err
 	}
-
 	err = vp.Unmarshal(&config)
 	if err != nil {
-		return Config{}, err
+		return ConfigMap{}, err
+	}
+	
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	if user == "" || pass == "" {
+		log.Fatalln("DB_USER and DB_PASSWORD must be set in the environment. (or in the config file)")
+	}
+	config.Surreal.User = user
+	config.Surreal.Password = pass
+	
+
+	return config, nil
+}
+
+func LoadUsersMap() (UserMap, error) {
+	vp := viper.New()
+	vp.SetConfigFile(getUsersPath())
+
+	var users UserMap
+	err := vp.ReadInConfig()
+	if err != nil {
+		return UserMap{}, err
+	}
+
+	err = vp.Unmarshal(&users)
+	if err != nil {
+		return UserMap{}, err
+	}
+
+	return users, nil
+}
+
+func LoadConfig() (Config, error) {
+	log.Info("Loading config...")
+
+
+	configMap, err := LoadConfigMap()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Infof("Loaded config: %+v", configMap)
+
+	userMap, err := LoadUsersMap()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Info("Loaded users")
+
+	config := Config{
+		Port: configMap.Port,
+		Users: userMap.Users,
+		Surreal: configMap.Surreal,
 	}
 
 	return config, nil
