ecea3a0e10603534c6bce4427c860d3a7ebd9ce2
awlsring <mattcanemail@gmail.com>
2023-01-13 17:02:49 -0800

    docs: update examples

--- examples/deployment/cdk8s/main.ts
+++ examples/deployment/cdk8s/main.ts
@@ -1,6 +1,6 @@
 import { Construct } from 'constructs';
 import { App, Chart, ChartProps } from 'cdk8s';
-import { ConfigMap, Deployment, Service, ServiceType, Volume } from 'cdk8s-plus-25';
+import { ConfigMap, Deployment, EnvValue, Secret, Service, ServiceType, Volume } from 'cdk8s-plus-25';
 import path = require('path');
 
 export class TerraformBackendSurrealChart extends Chart {
@@ -11,15 +11,29 @@ export class TerraformBackendSurrealChart extends Chart {
 
     const configVolume = Volume.fromConfigMap(this, "config-volume", config);
 
+    const secret = Secret.fromSecretName(this, "users", "users");
+    const db = Secret.fromSecretName(this, "dbcreds", "dbcreds"); // Secret with user and password
+    const usersVol = Volume.fromSecret(this, "users-volume", secret); // Secret with users
+
     const deployment = new Deployment(this, "deployment", {
       containers: [
         {
           image: "ghcr.io/awlsring/terraform-backend-surreal:latest",
           port: 8032,
+          envVariables: {
+            "CONFIG_PATH": EnvValue.fromValue("/config/config.yaml"),
+            "USERS_PATH": EnvValue.fromValue("/users/users.yaml"),
+            "DB_USER": EnvValue.fromSecretValue({secret: db, key: "user"}),
+            "DB_PASSWORD": EnvValue.fromSecretValue({secret: db, key: "password"}),
+          },
           volumeMounts: [
             {
               path: "/config",
               volume: configVolume,
+            },
+            {
+              path: "/users",
+              volume: usersVol,
             },
           ],
           securityContext: {
